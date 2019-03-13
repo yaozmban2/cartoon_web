f0fbdf2faa2ae9206a46cd654ee64581fb6db7f7
yaozmban2 <455766338@qq.com>
2019-03-13 18:39:50 +0800

    配置好了Swagger2文档框架

--- src/main/java/cn/yu/cartoon/cartoon_web/CartoonWebApplication.java
+++ src/main/java/cn/yu/cartoon/cartoon_web/CartoonWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("cn.yu.cartoon.cartoon_web.mapper")
 public class CartoonWebApplication {
 
     public static void main(String[] args) {
